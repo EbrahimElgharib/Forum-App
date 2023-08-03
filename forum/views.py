90305c38ec6a8303e29f9cbc5e73c9f42a544e6c
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-08-03 08:33:56 +0300

    shows answers in question detail

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Answer
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 # Create your views here.
 
@@ -23,6 +23,18 @@ class QuestionCreate(CreateView):
     
 class QuestionDatail(DetailView):
     model = Question
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        q = self.object
+        context['answers'] = Answer.objects.filter(
+            question=q
+        )
+
+        return context
+
     
     template_name = 'forum/question_details.html'
     
